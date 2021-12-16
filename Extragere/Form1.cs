e2759d5cbca5889139d58d9245326417dc9a2271
effinfon <effinfon@gmail.com>
2021-12-16 21:25:45 +0200

    The parser for the subjects has been completed. (untested)\nThe parser for the students is work in progress.\nIntend to structure data in class Student, class Classroom, class ExtractionMethod - there can be many extraction methods, and they can also integrate more compelexity, such as the specific constraints to use when extracting.

--- Extragere/Form1.cs
+++ Extragere/Form1.cs
@@ -109,11 +109,6 @@ namespace Extragere
                 richTextBox.Text += line + "\n";
             }
         }
-
-
-
-
-
         bool isOnlyWhiteSpace(string text) {
             for (int i = 0; i < text.Length; i++)
             {
@@ -125,12 +120,7 @@ namespace Extragere
         }
 
 
-        string extractStudentName__intolerant(string line) {
-            return "STUDENT";
-        }
-        string extractStudentName__tolerant(string line) {
-            return "STUDENT";
-        }
+        
 
 
         // Browse File - let's the user find a file to open
@@ -306,4 +296,460 @@ namespace Extragere
         
     }
 
+    class Student {
+        public int id;
+        public string family_name;
+        public string personal_name;
+        public bool[] group;   // this is read from file
+        public bool[] individual;  // init false; the size of the list is dependent on how many subjects there are
+
+        public Student(int id, string family_name, string personal_name, bool[] group, bool[] individual) {
+            this.id = id;
+            this.family_name = family_name;
+            this.personal_name = personal_name;
+
+            if (group.Length != individual.Length) { 
+                // <error>: this shouldn't happen, but how are you going to give up this function when
+                // it's a constructor ? it always returns a Student, unless it throws an exception,
+                // which I will not do -> so, it could return an invalid state of Student; but how would
+                // the type system know that ?
+            }
+
+            for (int index = 0; index < group.Length; index++) {
+                this.group[index] = group[index];
+            }
+
+            for (int index = 0; index < individual.Length; index++) {
+                this.individual[index] = individual[index];
+            }
+        }
+
+        //public bool addGroupProject(int project)
+        //{
+        //    if (!group_subjects.Contains(project))
+        //    {
+        //        group_subjects.Add(project);
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //public bool addIndividualProject(int project)
+        //{
+        //    if (!individual_subjects.Contains(project))
+        //    {
+        //        individual_subjects.Add(project);
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //string toString()
+        //{
+        //    string result = this.name;
+
+
+        //    result += "\nProiect(e) de grup: [";
+        //    for (int i = 0; i < this.group_subjects.Count; i++)
+        //    {
+        //        result += this.group_subjects[i];
+
+        //        if (i < this.group_subjects.Count - 1)
+        //        {
+        //            result += ", ";
+        //        }
+        //    }
+        //    result += "]";
+
+
+        //    result += "\nProiect(e) individual(e): [";
+        //    for (int i = 0; i < this.individual_subjects.Count; i++)
+        //    {
+        //        result += this.individual_subjects[i];
+
+        //        if (i < this.individual_subjects.Count - 1)
+        //        {
+        //            result += ", ";
+        //        }
+        //    }
+        //    result += "]";
+
+
+        //    return result;
+        //}
+
+        //string toString(List<string> subject_names)
+        //{
+        //    string result = this.name;
+
+
+        //    result += "\nProiect(e) de grup: [";
+        //    for (int i = 0; i < this.group_subjects.Count; i++)
+        //    {
+        //        if (i < subject_names.Count)
+        //        {
+        //            result += $"\"{subject_names[this.group_subjects[i]]}\"";
+        //        }
+        //        else
+        //        {
+        //            result += "(Subject's name is not available)";
+        //        }
+
+        //        if (i < this.group_subjects.Count - 1)
+        //        {
+        //            result += ", ";
+        //        }
+        //    }
+        //    result += "]";
+
+
+        //    result += "\nProiect(e) individual(e): [";
+        //    for (int i = 0; i < this.individual_subjects.Count; i++)
+        //    {
+        //        if (i < subject_names.Count)
+        //        {
+        //            result += $"\"{subject_names[this.individual_subjects[i]]}\"";
+        //        }
+        //        else
+        //        {
+        //            result += "(Subject's name is not available)";
+        //        }
+
+        //        if (i < this.individual_subjects.Count - 1)
+        //        {
+        //            result += ", ";
+        //        }
+        //    }
+        //    result += "]";
+
+
+        //    return result;
+        //}
+    
+    };
+
+//    class Student_allocatedSubjects -> I could further decompose this as I would in an SQL model
+
+    class Classroom {
+        // takes the subjects and all the needed files, processes them in the correct order
+        // and then creates the (classroom of) students
+        List<Tuple<int, string>> subjects;
+        List<Student> students;
+
+        public Classroom(string subjects_string, string students_string) {
+            // parse subjects from string
+            subjects = parseSubjects(subjects_string);
+
+
+            // sample Student
+            int id = 1;
+            string family_name = "";
+            string personal_name = "";
+            bool[] group = { false, true, false, false, false, false, false, false, false, true};
+            bool[] individual = { false, false, true, false, false, true, true, true, false, false};
+
+            Student student = new Student(id, family_name, personal_name, group, individual);
+
+
+            // parse student_names from string (might be empty, simply wrong, etc.) - it is not essential,
+            // but the number of students IS essential (if nothing else)
+            students = parseStudents(students_string);
+
+        }
+
+
+        List<Tuple<int, string>> parseSubjects(string subjects_string) {
+        // <TODO> design resilience for premature termination
+        // <TODO> design resilience for file ending in many line delimiters, or containing many contiguous
+            // line delimiters anywhere in its content
+        // <TODO> ..
+
+            List<Tuple<int, string>> result = new List<Tuple<int, string>>();
+
+            // the loop for a line-parsing state-machine
+            for (int index = 0; index < subjects_string.Length;) {
+                string id_string= "";
+                string name = "";
+
+                if (index < subjects_string.Length) {
+                    // [ \t]*
+                    while (index < subjects_string.Length && Util.isSpace(subjects_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [0-9]* -> a number is expected though, and prefered
+                    while (index < subjects_string.Length && Util.isDecimal(subjects_string[index])) {
+                        id_string += subjects_string[index];
+                        index++;
+                    }
+
+                    int id;
+                    if (id_string.Length > 0)
+                        int.TryParse(id_string, out id);
+                    else
+                        id = 0;
+
+                    // [ \t]*
+                    while (index < subjects_string.Length && Util.isSpace(subjects_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [.-]? -> it might also just be separated by ' ' or by '-'
+                    if (index < subjects_string.Length && Util.isSeparator_Id_Title(subjects_string[index])) {
+                        index++;
+                    }
+
+                    // [ \t]*
+                    while (index < subjects_string.Length && Util.isSpace(subjects_string[index])) {
+                        index++;
+                    }
+
+                    // [{text_Romanian}]*
+                    while (index < subjects_string.Length && Util.isText_Romanian(subjects_string[index])) {
+                        name += subjects_string[index];
+                        index++;
+                    }
+
+                    if(name.Length > 0) {
+                        result.Add(new Tuple<int, string>(id, name));
+                    }
+
+                    // [\n]*
+                    while (index < subjects_string.Length && Util.isLineDelimiter(subjects_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // the state-machine for parsing a line terminates here
+                }
+
+            }
+
+            return result;
+        }
+        string extractStudentName__intolerant(string line)
+        {
+            return "STUDENT";
+        }
+        string extractStudentName__tolerant(string line)
+        {
+            return "STUDENT";
+        }
+        List<Student> parseStudents(string students_string) {
+            // (*?): a rather important aspect about this parser state-machine is what does it do when
+            // it "doesn't match correctly"; does it consume (up to that point or to the end) or does it revert ?
+            // does it try to tolerate and always try and return something, in worst case returning some defaults ?
+            // does it return a ~ "state - fault / mismatch encountered" ? does it perhaps get stuck in an infinite
+            // loop or does it eventually reach the end of the string, unconditionally ?
+            // ..
+
+            List<Student> result = new List<Student>();
+
+            // the loop for a line-parsing state-machine
+            for (int index = 0; index < students_string.Length;)
+            {
+                string id_string = "";
+                string family_name = "";
+                string personal_name = "";
+
+                if (index < students_string.Length)
+                {
+                    // [ \t]*
+                    while (index < students_string.Length && Util.isSpace(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [0-9]* -> a number is expected though, and prefered
+                    while (index < students_string.Length && Util.isDecimal(students_string[index]))
+                    {
+                        id_string += students_string[index];
+                        index++;
+                    }
+
+                    int id;
+                    if (id_string.Length > 0)
+                        int.TryParse(id_string, out id);
+                    else
+                        id = 0;
+
+                    // [ \t]*
+                    while (index < students_string.Length && Util.isSpace(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [.-]? -> it might also just be separated by ' ' or by '-'
+                    if (index < students_string.Length && Util.isSeparator_Id_Title(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [ \t]*
+                    while (index < students_string.Length && Util.isSpace(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // ([{uppercase_letter_Romanian}] [{lowercase_letter_Romanian}]* [ \t]* [-]{0, 1})*
+                    // -> 0 switches to personal name, 1 extends the family name
+// (or use a) label: -> the thing is, this is hard to structure using just a while() and if(); ah, but there
+    // is also break, so this can actually work fine
+// (*): if(), while(), break - basis of operations for compositioning
+// (*): for now, the way the state-machine reacts to pattern mismatch is to simply skip and attempt the next part
+    // of the system of patterns (which are "expressed in the string", so to speak, as would be the case in a regex)
+                    
+                    while (index < students_string.Length && Util.isUpperCase_Romanian(students_string[index]))
+                    {
+                        if (index < students_string.Length && Util.isUpperCase_Romanian(students_string[index]))
+                        {
+                            family_name += students_string[index];
+                            index++;
+
+
+                            while (index < students_string.Length && Util.isLowerCase_Romanian(students_string[index]))
+                            {
+                                family_name += students_string[index];
+                                index++;
+                            }
+
+                            // (*): take this more as a sample for ideas; it is not actually necessary here, in this case
+                            //if (index < students_string.Length && Util.isSpace(students_string[index])) {
+                            //    family_name += ' ';
+                            //    index++;
+                            //}
+
+                            while (index < students_string.Length && Util.isSpace(students_string[index]))
+                            {
+                                index++;
+                            }
+
+                            // break depending on the -'s appearance or not
+                            if (index < students_string.Length && Util.isSeparator_IntraFamilyName(students_string[index]))
+                            {
+                                family_name += students_string[index];
+                                index++;
+// (possibly handled - POST, but correctly ?) <TODO #family_name>: unhandled case -> there is a '-' but there is no uppercase 
+    // romanian letter following, so it should actually remove the right-trailing '-'
+
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                            while (index < students_string.Length && Util.isSpace(students_string[index]))
+                            {
+                                index++;
+                            }
+                        }
+                        else 
+                        {
+                            break;
+                        }
+                    }
+
+                    while (family_name.EndsWith("-")) {
+                        family_name = family_name.Substring(0, family_name.Length - 1);
+                    }
+
+
+                    // ([{uppercase_letter_Romanian}] [{lowercase_letter_Romanian}]+ [ \t]* [-]{0, 1} [ \t]*)*
+                    // -> 0 switches to group projects, 1 extends the personal name
+
+                    while (index < students_string.Length && Util.isUpperCase_Romanian(students_string[index]))
+                    {
+                        if (index < students_string.Length && Util.isUpperCase_Romanian(students_string[index]))
+                        {
+                            personal_name += students_string[index];
+                            index++;
+
+
+                            while (index < students_string.Length && Util.isLowerCase_Romanian(students_string[index]))
+                            {
+                                personal_name += students_string[index];
+                                index++;
+                            }
+
+                            while (index < students_string.Length && Util.isSpace(students_string[index]))
+                            {
+                                index++;
+                            }
+
+                            // break depending on the -'s appearance or not
+                            if (index < students_string.Length && Util.isSeparator_IntraFamilyName(students_string[index]))
+                            {
+// <TODO #personal_name>: same as <TODO #family_name>
+                                personal_name += students_string[index];
+                                index++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                            while (index < students_string.Length && Util.isSpace(students_string[index]))
+                            {
+                                index++;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    while (personal_name.EndsWith("-"))
+                    {
+                        personal_name = personal_name.Substring(0, personal_name.Length - 1);
+                    }
+
+                    // [ \t]*
+                    while (index < students_string.Length && Util.isSpace(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // [<][0-9]*[>] | [(][0-9]*[)] | [\[][0-9]*[\]] | [{][0-9]*[}] 
+
+
+
+
+                    if (family_name.Length > 0 && personal_name.Length > 0)
+                    {
+                        //                        result.Add(new Tuple<int, string>(id, name));
+                    }
+
+                    // [\n]*
+                    while (index < students_string.Length && Util.isLineDelimiter(students_string[index]))
+                    {
+                        index++;
+                    }
+
+                    // the state-machine for parsing a line terminates here
+                }
+
+            }
+
+            return result;
+        }
+    };
+    class ExtractionMethod {
+        // the extraction method might extended the complexity that is composed and can be read from files, such
+        // as the possibility to specify new constraints
+
+        // an extraction method might involve multiple classrooms
+
+        // maybe scheduling could also be done similarly, with a nice GUI
+
+        Classroom classroom;
+    };
 }
---
+++ Extragere/Util.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extragere
+{
+    class Util
+    {
+        public static bool isSeparator_Id_Title(char symbol_ASCII)
+        {
+            return symbol_ASCII == '.' || symbol_ASCII == '-';
+        }
+
+        public static bool isSpace(char symbol_ASCII)
+        {
+            return symbol_ASCII == ' ' || symbol_ASCII == '\t';
+        }
+
+        public static bool isDecimal(char symbol_ASCII) {
+            return '0' <= symbol_ASCII && symbol_ASCII <= '9';
+        }
+
+        public static bool isText_English(char symbol_ASCII)
+        {
+            return (' ' <= symbol_ASCII && symbol_ASCII <= '~');
+        }
+
+        public static bool isText_Romanian(char symbol_ASCII)
+        {
+            return  (' ' <= symbol_ASCII && symbol_ASCII <= '~') ||
+                    symbol_ASCII == 'ă' || symbol_ASCII == 'Ă' ||
+                    symbol_ASCII == 'â' || symbol_ASCII == 'Â' ||
+                    symbol_ASCII == 'î' || symbol_ASCII == 'Î' ||
+                    symbol_ASCII == 'ș' || symbol_ASCII == 'Ș' ||
+                    symbol_ASCII == 'ț' || symbol_ASCII == 'Ț';
+        }
+
+        public static bool isName_Romanian(char symbol_ASCII)
+        {
+            // (*): optimizations such as "upper case vs lower case" being checked using one bit check
+            // are only relevant depending on the encoding (!); the ordering of the space takes precedence
+            // for naive, (!) but robust solutions
+            return  ('a' <= symbol_ASCII && symbol_ASCII <= 'z') ||
+                    ('A' <= symbol_ASCII && symbol_ASCII <= 'Z') ||
+                    symbol_ASCII == 'ă' || symbol_ASCII == 'Ă' ||
+                    symbol_ASCII == 'â' || symbol_ASCII == 'Â' ||
+                    symbol_ASCII == 'î' || symbol_ASCII == 'Î' ||
+                    symbol_ASCII == 'ș' || symbol_ASCII == 'Ș' ||
+                    symbol_ASCII == 'ț' || symbol_ASCII == 'Ț';
+        }
+
+        public static bool isLineDelimiter(char symbol_ASCII) {
+            return symbol_ASCII == '\n';
+        }
+    }
+}
